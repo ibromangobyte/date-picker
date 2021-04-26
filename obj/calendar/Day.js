b309508b9442fd46212eacc1dfd37054e11bc529
Ibrahim Tayib <ibrahimtayib@Ibrahims-MacBook-Pro.local>
2021-04-26 20:45:50 +1000

    Updated Day.js

--- obj/calendar/Day.js
+++ obj/calendar/Day.js
@@ -2,6 +2,25 @@
  * Day object
  */
 
+// Determine the number of weeks from the start of the year til today
+function getWeekNumber(date) {
+    const firstDayOfTheYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear = (date - firstDayOfTheYear) / 86400000;
+
+    return Math.ceil((pastDaysOfYear + firstDayOfTheYear.getDay() + 1) /7 ); 
+}
+
+// Determine whether the year is a leap year
+function isLeapYear(year) {
+    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+}
+
+/**
+ * Day object
+ * @params : date (optional)
+ * @params : lang (optional)
+ */
+
 class Day {
     constructor(date = null, lang = 'default') {
         date = date ?? new Date();
@@ -17,7 +36,33 @@ class Day {
         );
         this.month = date.toLocaleString(lang, { month: 'long' });
         this.monthShort = date.toLocaleString(lang, { month: 'short' });
-        this.timestamp = date.getTime;
+        this.timestamp = date.getTime();
+        this.week = getWeekNumber(date);
+    }
+
+    get isToday(){
+        return this.isEqualTo(new Date());
+    }
+
+    isEqualTo(date) {
+        date = date instanceof Day ? date.Date : date;
+        
+        return date.getDate() == this.date && 
+            date.getMonth() === this.monthNumber - 1 &&
+            date.getFullYear() === this.year;
+    }
+
+    format(formatStr) {
+        return formatStr
+            .replace(/\bYYYY\b/, this.year)
+            .replace(/\bYYY\b/, this.yearShort)
+            .replace(/\bWW\b/, this.week.toString().padStart(2, '0'))
+            .replace(/\bW\b/, this.week)
+            .replace(/\bDDDD\b/, this.day)
+            .replace(/\bDDD\b/, this.dayShort)
+            .replace(/\bDD\b/, this.date.toString().padStart(2, '0'))
+            .replace(/\bDDDD\b/, )
+            .replace(/\bDDDD\b/, )
     }
 }
 
